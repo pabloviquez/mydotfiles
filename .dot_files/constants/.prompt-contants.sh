94c934f629466a16ab502a8d60cdf4b64b61c52f
Pablo Viquez <pviquez@pabloviquez.com>
2014-11-25 12:07:30 -0600

    Adding initial files for dot files

---
+++ .dot_files/constants/.prompt-contants.sh
@@ -0,0 +1,15 @@
+# Various variables you might want for your PS1 prompt instead
+Time12h="\T"
+Time12a="\@"
+PathShort="\w"
+PathFull="\W"
+NewLine="\n"
+Jobs="\j"
+
+# Added by Pablo Viquez
+# Old PS1
+# PS1="\h:\W \u\$
+CurUser="\u"
+CurHost="\h"
+PrSpace=" "
+
